43ba9792d232ef2bcff01812260464d4b6ce1581
Tejas Ahluwalia <tejas@tujux.com>
2024-08-26 06:41:44 +0530

    use godotenv to load .env

--- internal/config/main.go
+++ internal/config/main.go
@@ -2,37 +2,17 @@ package config
 
 import (
 	"os"
-	"strings"
 )
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 
 type Config map[string]string
 
 func init() {
-	DOTENV_PATH := "./.env"
-	dat, err := os.ReadFile(DOTENV_PATH)
-	check(err)
-
 	config := make(Config)
 
 	expected_keys := []string{"CLIENT_ID", "CLIENT_SECRET", "APP_ID", "APP_SECRET", "WEBHOOK_VERIFY_TOKEN", "ACCOUNT_TOKEN", "ACCOUNT_ID", "PAGE_ACCESS_TOKEN", "TURSO_DATABASE_URL", "TURSO_AUTH_TOKEN"}
 
-	lines := strings.Split(string(dat), "\n")
-	for _, line := range lines {
-		parts := strings.Split(line, "=")
-		if len(parts) == 2 {
-			key := parts[0]
-			value := parts[1]
-			config[key] = value
-		}
-	}
-
 	for _, key := range expected_keys {
+		config[key] = os.Getenv(key)
 		if _, ok := config[key]; !ok {
 			panic("Missing key in .env file: " + key)
 		}
