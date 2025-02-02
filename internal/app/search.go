dcdaa2cd0a15a6e31f072d35dfc54e50cc21b46f
Tejas Ahluwalia <tejas@tujux.com>
2025-02-02 07:15:14 +0530

    strip @ prefix on search

--- internal/app/search.go
+++ internal/app/search.go
@@ -17,6 +17,7 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	} else {
+		username = strings.TrimPrefix(username, "@")
 		safeUsername := url.QueryEscape(username) // import "net/url"
 		http.Redirect(w, r, "/profile/"+safeUsername, http.StatusFound)
 		return
