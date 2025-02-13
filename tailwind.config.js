b43d12cf2c56b833e1508e87ea3918203a3f5b87
Tejas Ahluwalia <tejasahluwalia@gmail.com>
2025-02-13 07:35:39 +0530

    tmp tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.templ"],
-  theme: {
-    extend: {
-
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
-  ],
-}
-
+	content: ["./**/*.templ"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
